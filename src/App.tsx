549fab510426078c8aa2c44724e9b787fdedb1f9
Carlos Oliveira <chopcsom@gmail.com>
2021-03-17 01:05:35 -0300

    Feat: adds Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <div className="App">
-      <h1>HELLO</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
   );
 }
